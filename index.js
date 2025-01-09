c8a0ce9e97ae98f265d07c27f9c0ca26de12b9ca
Rayhaan1905 <rayhaansam21@gmail.com>
2025-01-09 09:56:47 +0000

    Add styles

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
-
+// Add animations or interactivity
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page loaded");
+  });
+  
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",(req, res)=>{
     res.sendFile(path.join(__dirname, 'views', "index.html"))
 });
