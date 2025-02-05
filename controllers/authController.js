80110fa927f7baf149942a1f681358fd32f84176
Rayhaan1905 <rayhaansam21@gmail.com>
2025-02-05 15:19:45 +0000

    Add user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,12 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 const register = async (req, res) => {
+    try {
         const {username, password1,password2}= req.body;
         if(password1 !== password2)
         {
             console.log("The passwords you entered do not match.");
-            res.send({code :400, message:'The passwords you entered do not match'});
+            res.status(400).json({ok: false, message:'The passwords you entered do not match'});
         }
         else{
             const pswd = await bcrypt.hash(password1,10);
@@ -16,8 +17,13 @@ const register = async (req, res) => {
                 username: username,
                 passwordHash: pswd
             });
-            user.save();
+            await user.save();
+            res.status(201).json({ok: true, message: 'User registered successfully'});
         }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ok: false, message: 'Internal server error'});
+    }
 }
 
 
