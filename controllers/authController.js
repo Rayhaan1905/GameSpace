a8d52f02bd93629bb63b5786e9a2dc742baeb48c
Rayhaan1905 <rayhaansam21@gmail.com>
2025-02-05 15:49:19 +0000

    Add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../model/user');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     try {
@@ -25,9 +26,38 @@ const register = async (req, res) => {
         res.status(500).json({ok: false, message: 'Internal server error'});
     }
 }
-
-
 router.post('/register', register);
 
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username: username });
+        if (!user) {
+            console.log("User not found");
+            return res.status(401).json({ ok: false, message: 'Invalid credentials' });
+        }
+        
+        const match = await bcrypt.compare(password, user.passwordHash);
+        if (match) {
+            const token = jwt.sign({ username: user.username }, process.env.SECRET, { expiresIn: '1h' });
+
+            res.cookie('token', token, {
+                httpOnly: true,       // prevents JS from reading the cookie
+                secure: false,        // allow HTTP
+                sameSite: 'Strict',   // cookie is sent only to the same site
+                maxAge: 3600000 * 24  // 1 day
+            });
+
+            res.status(200).json({ ok: true, message: 'User logged in successfully' });
+        } else {
+            console.log("Invalid password");
+            res.status(401).json({ ok: false, message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ ok: false, message: 'Internal server error' });
+    }
+}
+router.post('/login', login);
 
 module.exports = router;
